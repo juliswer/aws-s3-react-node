fe63417fd7c82ae681d3dd47630352f500b110f3
juliswer <julianswer36@gmail.com>
2022-09-03 18:52:59 -0300

    uploading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
 function App() {
+  const [post, setPost] = React.useState({
+    title: "",
+    photo: null,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(post);
+  };
+
   return (
     <div>
       <form>
-        <input type="text" placeholder="title" name="title" />
-        <input type="file" name="photo" onChange={e => console.log(e)} />
-        <button>Upload</button>
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+        />
+        <input
+          type="file"
+          name="photo"
+          onChange={(e) => setPost({ ...post, photo: e.target.files[0] })}
+        />
+        <button onClick={handleSubmit}>Upload</button>
       </form>
     </div>
   );
