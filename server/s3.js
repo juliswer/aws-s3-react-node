7220f80ca0e0a8c505f287744847ec88876a48c9
juliswer <julianswer36@gmail.com>
2022-09-04 14:43:20 -0300

    env and s3

---
+++ server/s3.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { S3Client } = require("@aws-sdk/client-s3");
+
+const AWS_PUBLIC_KEY = process.env.AWS_PUBLIC_KEY;
+const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION;
+
+const client = new S3Client({
+  region: AWS_BUCKET_REGION,
+});
