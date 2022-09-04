7164f8450397bb3a5eb6e49d292b26a5bc6764f8
juliswer <julianswer36@gmail.com>
2022-09-04 00:40:50 -0300

    uploading back

--- server/photos.routes.js
+++ server/photos.routes.js
@@ -9,17 +9,24 @@ router.get("/", (req, res) => {
 });
 
 router.post("/upload", (req, res) => {
-  const uploadedFile = req.files.photo;
-  console.log(uploadedFile);
-  // console.log(req.files.photo.name);
-  res.json({
-    success: true,
-    file: {
-      name: uploadedFile.name,
-      type: uploadedFile.mimetype,
-      size: `${(uploadedFile.size / 1000000).toFixed(2)}Mb`,
-    },
-  });
+  try {
+    const uploadedFile = req.files.photo;
+    res.json({
+      success: true,
+      file: {
+        name: uploadedFile.name,
+        type: uploadedFile.mimetype,
+        size: `${(uploadedFile.size / 1000000).toFixed(2)}Mb`,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      error: error.message,
+      file: undefined,
+    });
+  }
 });
 
 module.exports = router;
